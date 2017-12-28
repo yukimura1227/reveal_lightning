162641d5bab8d6f68de6c2e63930099978cbce55
yukimura1227 <takamura1227@gmail.com>
2017-12-28 23:37:25 +0900

    direct image paste function

--- convert.js
+++ convert.js
@@ -2,11 +2,32 @@ const $ = require('jquery');  // jQuery now loaded and assigned to $// enable ev
 const marked = require('marked');
 const fs = require('fs');
 const target_md_file = 'sample.md';
+const clipboard = require('electron').clipboard
 
 $('#js-markdown-input').on('change', function() {
   compile_and_display_markdown();
   write_file(target_md_file);
 });
+
+$('#js-image-paste-btn').on('click', function() {
+  var image_file_name = new Date().getTime() + '.png';
+  var availableFormats = clipboard.availableFormats()
+  var image_data = clipboard.readImage().toPng();
+  console.log(image_data);
+  fs.writeFile(image_file_name, image_data, function (error) {
+    console.log(error);
+  });
+  // すぐにappendするとファイルが読み込めないので、waitしてからappend
+  setTimeout(
+    function () {
+      var input_area = $('#js-markdown-input');
+      input_area.val(input_area.val() + "![](" + image_file_name + ")\n");
+      $('#js-markdown-input').trigger('change');
+    },
+    500
+  );
+});
+
 
 function compile_and_display_markdown() {
   let input_markdown = $('#js-markdown-input').val();
