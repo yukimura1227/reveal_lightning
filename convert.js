2242cb325b558db12f03c4738f7d45c84c09bd82
yukimura1227 <takamura1227@gmail.com>
2017-12-28 14:24:44 +0900

    refactor constantize load target markdown file path

--- convert.js
+++ convert.js
@@ -1,18 +1,19 @@
 const $ = require('jquery');  // jQuery now loaded and assigned to $// enable everything
 const marked = require('marked');
 const fs = require('fs');
+const target_md_file = 'sample.md';
 
 $('#js-markdown-input').on('change', function() {
   let input_markdown = $('#js-markdown-input').val();
   let result_html = marked(input_markdown);
   $('#js-markdown-result').html(result_html);
 
-  write_file('sample.md')
+  write_file(target_md_file)
 
 });
 
 function initialize_load() {
-  read_file('sample.md');
+  read_file(target_md_file);
 }
 
 function read_file(target_filepath) {
@@ -21,7 +22,7 @@ function read_file(target_filepath) {
 }
 
 function write_file(target_filepath) {
-  fs.writeFile('sample.md', $('#js-markdown-input').val());
+  fs.writeFile(target_filepath, $('#js-markdown-input').val());
 }
 
 initialize_load();
