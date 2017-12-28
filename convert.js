79ae7ad3a302b1af1c27d75b81bbdbd615951c71
yukimura1227 <takamura1227@gmail.com>
2017-12-28 14:03:23 +0900

    auto save function

--- convert.js
+++ convert.js
@@ -1,13 +1,16 @@
 let $ = require('jquery');  // jQuery now loaded and assigned to $// enable everything
 var marked = require('marked');
+let fs = require('fs');
 
 $('#js-markdown-input').on('change', function() {
   var input_markdown = $('#js-markdown-input').val();
   var result_html = marked(input_markdown);
   $('#js-markdown-result').html(result_html);
+
+  fs.writeFile('sample.md', $('#js-markdown-input').val());
+
 });
 
-let fs = require('fs');
 function initialize_load() {
   let data = fs.readFileSync('sample.md', 'utf8');
   $('#js-markdown-input').val(data);
