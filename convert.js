820075ee6094db635703d5df51fb5f2d0f39071a
yukimura1227 <takamura1227@gmail.com>
2017-12-29 00:44:50 +0900

    using ace editor instead of standard textare

--- convert.js
+++ convert.js
@@ -3,13 +3,26 @@ const marked = require('marked');
 const fs = require('fs');
 const target_md_file = 'sample.md';
 
-$('#js-markdown-input').on('change', function() {
-  compile_and_display_markdown();
-  write_file(target_md_file);
+require('ace-min-noconflict');
+require('ace-min-noconflict/mode-markdown');
+
+var editor;
+window.addEventListener('DOMContentLoaded',function() {
+  editor = ace.edit("js-markdown-input");
+  editor.getSession().setMode('ace/mode/markdown');
+  editor.$blockScrolling = Infinity; // ワーニングに対処
+  if(process.platform == 'darwin') { // Ctrl+Pが効かない問題に対処
+    editor.commands.bindKey("Ctrl-P", "golineup");
+  }
+  editor.getSession().on('change', function() {
+    compile_and_display_markdown();
+    write_file(target_md_file);
+  });
+  initialize_load();
 });
 
 function compile_and_display_markdown() {
-  let input_markdown = $('#js-markdown-input').val();
+  let input_markdown = editor.getValue();
   let result_html = marked(input_markdown);
   $('#js-markdown-result').html(result_html);
 }
@@ -21,11 +34,9 @@ function initialize_load() {
 
 function read_file(target_filepath) {
   let data = fs.readFileSync(target_filepath, 'utf8');
-  $('#js-markdown-input').val(data);
+  editor.setValue(data);
 }
 
 function write_file(target_filepath) {
-  fs.writeFile(target_filepath, $('#js-markdown-input').val());
+  fs.writeFile(target_filepath, editor.getValue());
 }
-
-initialize_load();
