a2ef266f58cc0ce8c986fdd42e950b716eff4a85
yukimura1227 <takamura1227@gmail.com>
2017-12-28 14:20:10 +0900

    refactor extract file read/write function

--- convert.js
+++ convert.js
@@ -7,13 +7,21 @@ $('#js-markdown-input').on('change', function() {
   var result_html = marked(input_markdown);
   $('#js-markdown-result').html(result_html);
 
-  fs.writeFile('sample.md', $('#js-markdown-input').val());
+  write_file('sample.md')
 
 });
 
 function initialize_load() {
-  let data = fs.readFileSync('sample.md', 'utf8');
+  read_file('sample.md');
+}
+
+function read_file(target_filepath) {
+  let data = fs.readFileSync(target_filepath, 'utf8');
   $('#js-markdown-input').val(data);
+}
+
+function write_file(target_filepath) {
+  fs.writeFile('sample.md', $('#js-markdown-input').val());
 }
 
 initialize_load();
