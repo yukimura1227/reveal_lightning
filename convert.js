70e5c43bf713f2369694b9cb9ae65366958c7cd4
yukimura1227 <takamura1227@gmail.com>
2017-12-29 18:23:33 +0900

    refactor [load library part] to setup.js

--- convert.js
+++ convert.js
@@ -1,12 +1,3 @@
-const $ = require('jquery');  // jQuery now loaded and assigned to $// enable everything
-const marked = require('marked');
-const fs = require('fs');
-const target_md_file = 'sample.md';
-const clipboard = require('electron').clipboard
-
-require('ace-min-noconflict');
-require('ace-min-noconflict/mode-markdown');
-
 var editor;
 window.addEventListener('DOMContentLoaded',function() {
   editor = ace.edit("js-markdown-input");
