60f5916cb3fd249bd8f20dbea4b804f6c31dd72d
yukimura1227 <takamura1227@gmail.com>
2017-12-29 18:11:37 +0900

    reactor separate utility functions

--- convert.js
+++ convert.js
@@ -52,11 +52,3 @@ function initialize_load() {
   compile_and_display_markdown();
 }
 
-function read_file(target_filepath) {
-  let data = fs.readFileSync(target_filepath, 'utf8');
-  editor.setValue(data);
-}
-
-function write_file(target_filepath) {
-  fs.writeFile(target_filepath, editor.getValue(), function (error) {});
-}
