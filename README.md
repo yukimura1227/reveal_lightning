43726feadab67396bf091f4ae5cd076d68224f54
yukimura1227 <takamura1227@gmail.com>
2022-02-16 18:14:32 +0900

    chore: yarn run dev:xxxで、mainプロセス用のtsc, rendererプロセス用のwebpack、electron本体を動かせるようにscriptを追加

---
+++ README.md
@@ -0,0 +1,11 @@
+# RevealLightning
+
+## Development
+
+### Execution
+
+```
+yarn run dev:tsc
+yarn run dev:webpack
+yarn run dev:electron
+```
---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import path from 'path';
+import { BrowserWindow, app, session } from 'electron';
+import { searchDevtools } from 'electron-search-devtools';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+// 開発モードの場合はホットリロードする
+if (isDev) {
+  const execPath =
+    process.platform === 'win32'
+      ? '../node_modules/electron/dist/electron.exe'
+      : '../node_modules/.bin/electron';
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('electron-reload')(__dirname, {
+    electron: path.resolve(__dirname, execPath),
+  });
+}
+
+// BrowserWindow インスタンスを作成する関数
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.resolve(__dirname, 'preload.js'),
+    },
+  });
+
+  if (isDev) {
+    // 開発モードの場合はデベロッパーツールを開く
+    mainWindow.webContents.openDevTools({ mode: 'detach' });
+  }
+
+  // レンダラープロセスをロード
+  mainWindow.loadFile('dist/index.html');
+};
+
+app.whenReady().then(async () => {
+  if (isDev) {
+    // 開発モードの場合は React Devtools をロード
+    const devtools = await searchDevtools('REACT');
+    if (devtools) {
+      await session.defaultSession.loadExtension(devtools, {
+        allowFileAccess: true,
+      });
+    }
+  }
+
+  // BrowserWindow インスタンスを作成
+  createWindow();
+});
+
+// すべてのウィンドウが閉じられたらアプリを終了する
+app.once('window-all-closed', () => app.quit());
